d1024f90958df58a4db162c439574241c80c5afb
Dmitry Barskov <barskoidim@gmail.com>
2018-10-18 21:20:09 +0300

    create methods for SequenceIterator

--- sequence_iterator.rb
+++ sequence_iterator.rb
@@ -1,3 +1,16 @@
 class SequenceIterator
+  def initialize
+    @state = [1]
+  end
+
+  def state
+    @state.join
+  end
+
+  def next
+    # todo
+    # implement method
+    # 1 -> 11 -> 21 -> 1211 -> 111221 -> ...
+  end
 end
 
