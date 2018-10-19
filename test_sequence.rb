60b3f2ab1083fff8d3c1e64cad30f90042f6af9b
Dmitry Barskov <barskoidim@gmail.com>
2018-10-19 07:24:31 +0300

    create tests for Sequence

--- test_sequence.rb
+++ test_sequence.rb
@@ -2,5 +2,23 @@ require 'test/unit'
 require './sequence.rb'
 
 class TestSequence < Test::Unit::TestCase
+
+  def setup
+    @sequence = Sequence.new 11
+  end
+
+  def test_indexing
+    assert_equal '1', @sequence[0]
+    assert_equal '11', @sequence[1]
+    assert_equal '21', @sequence[2]
+    assert_equal '1211', @sequence[3]
+    assert_equal '111221', @sequence[4]
+    assert_equal '312211', @sequence[5]
+    assert_equal '13112221', @sequence[6]
+    assert_equal '1113213211', @sequence[7]
+    assert_equal '31131211131221', @sequence[8]
+    assert_equal '13211311123113112211', @sequence[9]
+    assert_equal '11131221133112132113212221', @sequence[10]
+  end
 end
 
