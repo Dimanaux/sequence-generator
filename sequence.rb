085440a48092a6e0120bdba6b8195bbd2e50faf6
Dmitry Barskov <barskoidim@gmail.com>
2018-10-19 07:26:13 +0300

    implement method [] for Sequence

--- sequence.rb
+++ sequence.rb
@@ -4,7 +4,25 @@ class Sequence
   def initialize(bound=16)
     @iter = SequenceIterator.new
     @bound = bound
-    @sequence = []
+    @sequence = [@iter.state]
+  end
+
+  def [](index)
+    if index < @bound
+      self.ensure index
+      @sequence[index]
+    else
+      raise "Index #{index} is out of bound #{@bound}!"
+    end
+  end
+
+  def ensure(index)
+    if @sequence.size <= index then
+      @iter.next
+      @sequence << @iter.state
+
+      self.ensure index
+    end
   end
 end
 
