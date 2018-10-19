e6e3e3d48a49163ce597f4ad40692f19d17ba1f5
Dmitry Barskov <barskoidim@gmail.com>
2018-10-19 19:14:17 +0300

    add test for sequence iter

---
+++ test_sequence_iterator.rb
@@ -0,0 +1,29 @@
+require './sequence_iterator.rb'
+require 'test/unit'
+
+# tests SequenceIterator
+class TestSequenceIterator < Test::Unit::TestCase
+  def setup
+    @iter = SequenceIterator.new
+  end
+
+  def test_iter
+    assert_equal '1', @iter.state
+
+    @iter.next
+    assert_equal '11', @iter.state
+
+    @iter.next
+    assert_equal '21', @iter.state
+
+    @iter.next
+    assert_equal '1211', @iter.state
+
+    @iter.next
+    assert_equal '111221', @iter.state
+    
+    @iter.next
+    assert_equal '312211', @iter.state
+  end
+end
+
